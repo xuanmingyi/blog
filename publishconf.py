7d95bad888f6a567e6f79abe8e224957c28e53be
樱宝宝 <xuanmingyi@gmail.com>
2015-04-09 23:53:34 +0800

    move site url to blog.xuanmingyi.com

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://xuanmingyi.github.com'
+SITEURL = 'http://blog.xuanmingyi.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
